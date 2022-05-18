b8214a068e8d7b5ce7e2a56e08e385170d22db28
JoaoKeuwe <joaov2872@hotmail.com>
2022-05-18 16:00:20 -0300

    test: :wheelchair: Testando função getAll, Products Controllers

---
+++ test/unit/controllers/productsControllers.js
@@ -0,0 +1,61 @@
+const sinon =require('sinon');
+const {expect} = require('chai');
+
+const productService =require('../../../services/productService');
+
+const productsController = require('../../../controllers/controllerProducts');
+
+describe('1-chamada do controller get all', () => {
+    describe('testando função getAll', async () => {
+        const req = {}
+        const res= {}
+
+        before(() => {
+            res.status =sinon.stub().returns(res)
+            res.json =sinon.stub().returns();
+            sinon.stub(productService, 'getAll').resolves([]);
+        })
+
+        after(() => {
+            productService.getAll.restore();
+        })
+        it('é chamado metodo "status" passando o codigo 200', async () => {
+            await productsController.getAll(req, res)
+            expect(res.status.calledWith(200)).to.be.equal(true); 
+        })
+
+        it('é chamado metodo "status" passando o codigo 400', async () => {
+            await productsController.getAll(req, res)
+            expect(res.status.calledWith(400)).to.be.equal(false);
+        })
+
+        it('é retornado um metodo json',async () => {
+            await productsController.getAll(req, res)
+
+            expect(res.json.calledWith(sinon.match.array)).to.be.equal(true);
+        })
+    })
+})
+
+describe('2-chamada do controller get all ERROR', () => {
+    describe('testando função  ERROR', async () => {
+        const req = {}
+        const res= {}
+
+        before(() => {
+            res.status =sinon.stub().returns(res)
+            res.json =sinon.stub().returns();
+            sinon.stub(productService, 'getAll').resolves(undefined);
+        })
+
+        after(() => {
+            productService.getAll.restore();
+        })
+        it('é chamado metodo "status" passando o codigo 400', async () => {
+            await productsController.getAll(req, res)
+            expect(res.status.calledWith(400)).to.be.equal(true);
+        })
+    })
+})
+
+
--- test/unit/models/salesModels.js
+++ test/unit/models/salesModels.js
@@ -42,7 +42,7 @@ describe('2- testando função findById', async () => {
 });
 
 describe('3- testando função getId', async () => {
-    const resultList = [{}]
+    const resultList = [[]]
         before(() => {
             sinon.stub(connection, 'execute')
             .resolves(resultList)
@@ -55,7 +55,62 @@ describe('3- testando função getId', async () => {
         it('retorna um array', async () => {
             const response = await salesModel.getId()
 
+            expect(response).to.be.a("array")
+        })
+});
+
+describe('4- testando função addSales', async () => {
+    const resultList = [[]]
+        before(() => {
+            sinon.stub(connection, 'execute')
+            .resolves(resultList)
+        })
+
+        after(() => {
+            connection.execute.restore()
+        });
+
+        it('retorna um array', async () => {
+            const response = await salesModel.addSales()
+
+            expect(response).to.be.a("array")
+        })
+});
+
+describe('5- testando função create', async () => {
+    const resultList = [{saleId: 4}]
+        before(() => {
+            sinon.stub(connection, 'execute')
+            .resolves(resultList)
+        })
+
+        after(() => {
+            connection.execute.restore()
+        });
+
+        it('retorna um objeto', async () => {
+            const response = await salesModel.create()
+
             expect(response).to.be.an("object")
         })
 });
+
+describe('6- testando função updateSale', async () => {
+    const resultList = [{saleId: 4}]
+        before(() => {
+            sinon.stub(connection, 'execute')
+            .resolves(resultList)
+        })
+
+        after(() => {
+            connection.execute.restore()
+        });
+
+        it('retorna um array', async () => {
+            const response = await salesModel.updateSale()
+
+            expect(response).to.be.an("object")
+        })
+});
+
 })
