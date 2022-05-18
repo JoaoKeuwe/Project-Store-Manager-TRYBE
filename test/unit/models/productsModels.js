5d062c230b1343b05f0d7d1e073b9b65588faa4e
JoaoKeuwe <joaov2872@hotmail.com>
2022-05-17 22:12:05 -0300

    test: :package: Testando função validName, productsModel

--- test/unit/models/productsModels.js
+++ test/unit/models/productsModels.js
@@ -4,24 +4,7 @@ const { expect } = require('chai');
 const connection = require('../../../models/connection')
 const producstModel = require('../../../models/productModel');
 describe('product Models ', () => {
-describe('1- testando função getAll', async () => {
-const resultList = [[]]
-    before(() => {
-        sinon.stub(connection, 'execute')
-        .resolves(resultList)
-    })
-
-    after(() => {
-        connection.execute.restore()
-    });
-
-    it('retorna um array', async () => {
-        const response = await producstModel.getAll()
-
-        expect(response).to.be.a("array")
-    })
-});
-describe('2- testando função findById', async () => {
+    describe('1- testando função getAll', async () => {
     const resultList = [[]]
         before(() => {
             sinon.stub(connection, 'execute')
@@ -31,16 +14,51 @@ describe('2- testando função findById', async () => {
         after(() => {
             connection.execute.restore()
         });
-    
+
         it('retorna um array', async () => {
-            const response = await producstModel.findById()
-    
+            const response = await producstModel.getAll()
+
             expect(response).to.be.a("array")
         })
-});
+    });
+    describe('2- testando função findById', async () => {
+        const resultList = [[]]
+            before(() => {
+                sinon.stub(connection, 'execute')
+                .resolves(resultList)
+            })
 
-describe('3- testando função createAll', async () => {
-        const resultList = [{insertId:3}]
+            after(() => {
+                connection.execute.restore()
+            });
+        
+            it('retorna um array', async () => {
+                const response = await producstModel.findById()
+        
+                expect(response).to.be.a("array")
+            })
+    });
+    describe('3- testando função createAll', async () => {
+            const resultList = [{insertId:3}]
+                before(() => {
+                    sinon.stub(connection, 'execute')
+                    .resolves(resultList)
+                })
+        
+                after(() => {
+                    connection.execute.restore()
+                });
+            
+                it('retorna um objeto', async () => {
+                    const name = 'keuwe'
+                    const quantity = 30
+                    const response = await producstModel.createAll(name, quantity)
+            
+                    expect(response).to.be.an("object")
+                })
+    })
+    describe('4- testando função validName', async () => {
+        const resultList = [[]]
             before(() => {
                 sinon.stub(connection, 'execute')
                 .resolves(resultList)
@@ -50,14 +68,13 @@ describe('3- testando função createAll', async () => {
                 connection.execute.restore()
             });
         
-            it('retorna um objeto', async () => {
-                const name = 'keuwe'
-                const quantity = 30
-                const response = await producstModel.createAll(name, quantity)
-        
-                expect(response).to.be.an("object")
+            it('retorna um array', async () => {
+            
+                const response = await producstModel.validName()
+                expect(response).to.be.a("array")
             })
 })
+    
 })
 
 
