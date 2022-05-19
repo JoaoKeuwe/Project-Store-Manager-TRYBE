ae43230f9e91170d0d7543dc814fd919aeb281de
JoaoKeuwe <joaov2872@hotmail.com>
2022-05-18 21:52:29 -0300

    test: :bulb: Tstando função create, controller sales

--- test/unit/controllers/salesControllers.js
+++ test/unit/controllers/salesControllers.js
@@ -32,3 +32,88 @@ describe('1-chamada do controller sales controller', () => {
         })
     })
 })
+describe('2-chamada do controller sales controller', () => {
+    describe('testando função getAll error', async () => {
+        const req = {}
+        const res= {}
+
+        before(() => {
+            res.status =sinon.stub().returns(res)
+            res.json =sinon.stub().returns();
+            sinon.stub(salesService, 'getAll').resolves(undefined);
+        })
+
+        after(() => {
+            salesService.getAll.restore();
+        })
+        it('é chamado metodo "status" passando o codigo 400', async () => {
+            await productSales.getAll(req, res)
+            expect(res.status.calledWith(400)).to.be.equal(true); 
+        })
+    })
+})
+
+describe('3-chamada do controller sales controller', () => {
+    describe('testando função findById ', async () => {
+        const req = {}
+        const res= {}
+        req.params = {id: 1}
+
+        before(() => {
+            res.status =sinon.stub().returns(res)
+            res.json =sinon.stub().returns();
+            sinon.stub(salesService, 'findById').resolves(true);
+        })
+
+        after(() => {
+            salesService.findById.restore();
+        })
+        it('é chamado metodo "status" passando o codigo 200', async () => {
+            await productSales.findById(req, res)
+            expect(res.status.calledWith(200)).to.be.equal(true); 
+        })
+    })
+})
+describe('4-chamada do controller sales controller', () => {
+    describe('testando função findById ERROR ', async () => {
+        const req = {}
+        const res= {}
+        req.params = {id: 1}
+
+        before(() => {
+            res.status =sinon.stub().returns(res)
+            res.json =sinon.stub().returns();
+            sinon.stub(salesService, 'findById').resolves([]);
+        })
+
+        after(() => {
+            salesService.findById.restore();
+        })
+        it('é chamado metodo "status" passando o codigo 400', async () => {
+            await productSales.findById(req, res)
+            expect(res.status.calledWith(400)).to.be.equal(true); 
+        })
+    })
+})
+
+describe('5-chamada do controller sales controller', () => {
+    describe('testando função createeeee ', async () => {
+        const req = {}
+        const res= {}
+        req.body =[]
+
+        before(() => {
+            res.status =sinon.stub().returns(res)
+            res.json =sinon.stub().returns();
+            sinon.stub(salesService, 'create').resolves(true);
+        })
+
+        after(() => {
+            salesService.create.restore();
+        })
+        it('é chamado metodo "status" passando o codigo 200', async () => {
+            await productSales.create(req, res)
+            expect(res.status.calledWith(201)).to.be.equal(true); 
+        })
+    })
+})
